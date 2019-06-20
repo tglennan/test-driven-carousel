83b9189ae8094a1a555039e7ad4d542423b298df
Tim Glennan <tim.glennan@tesserent.com>
2019-06-20 21:00:03 +1000

    :sparkles: Initial implementation of Carousel component

---
+++ src/Carousel.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import CarouselButton from './CarouselButton.js';
+import CarouselSlide from './CarouselSlide.js';
+
+class Carousel extends PureComponent {
+  static propTypes = {
+    slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
+      .isRequired,
+  };
+
+  state = {
+    slideIndex: 0,
+  };
+
+  handlePrevClick = () => {
+    const { slides } = this.props;
+    this.setState(({ slideIndex }) => ({
+      slideIndex: (slideIndex + slides.length - 1) % slides.length,
+    }));
+  };
+
+  handleNextClick = () => {
+    const { slides } = this.props;
+    this.setState(({ slideIndex }) => ({
+      slideIndex: (slideIndex + 1) % slides.length,
+    }));
+  };
+
+  render() {
+    const { slides, ...rest } = this.props;
+    return (
+      <div {...rest}>
+        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselButton data-action="prev" onClick={this.handlePrevClick}>
+          Prev
+        </CarouselButton>
+        <CarouselButton data-action="next" onClick={this.handleNextClick}>
+          Next
+        </CarouselButton>
+      </div>
+    );
+  }
+}
+
+Carousel.propTypes = {};
+
+export default Carousel;
--- src/tests/CarouselSlide.test.js
+++ src/tests/CarouselSlide.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CarouselSlide from '../CarouselSlide';
-import { wrap } from 'module';
 
 describe('CarouselSlide', () => {
   let wrapper;
